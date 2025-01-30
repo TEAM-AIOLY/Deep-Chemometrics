aa35d88f5ada3f68d0cb3bd850b4c3fe0e4f053e
florent-abdelghafour <124244994+florent-abdelghafour@users.noreply.github.com>
2025-01-30 13:58:23 +0100

    update config

--- write_config_bash_ViT1D.py
+++ write_config_bash_ViT1D.py
@@ -5,7 +5,7 @@ import json
 base_params = {
     "dataset_type": "nir",
     "data_path": "./data/dataset/Mango/mango_dm_full_outlier_removed2.mat",  
-    "y_labels": [],
+    "y_labels": ['dm_mango'],
     "batch_size": 1024,
     "num_epochs": 2000,
     "model_name": "_ViT1D_Mango",
@@ -33,8 +33,8 @@ y_labels_options = [
 # base_params["y_labels"] = y_labels
 # y_labels_short_str = y_labels[0][:5]
 
-y_lab = 'mango_dm'
-y_labels =y_lab
+y_lab='dm_mango'
+
 
 param_file = os.path.join(
     os.path.dirname(base_params['data_path']),'config',
