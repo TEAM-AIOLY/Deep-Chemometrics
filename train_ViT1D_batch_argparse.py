ff69aa61d2a365c56acbc5ec86e48af9fda6f46d
florent-abdelghafour <124244994+florent-abdelghafour@users.noreply.github.com>
2025-02-05 00:04:47 +0100

    Update train_ViT1D_batch_argparse.py

--- train_ViT1D_batch_argparse.py
+++ train_ViT1D_batch_argparse.py
@@ -103,171 +103,173 @@ if __name__ == "__main__":
         model=ViT_1D(mean = params['mean'], std = params['std'], seq_len = params['spec_dims'], patch_size = params['PS'], 
                     dim_embed = params['DE'], trans_layers = params['TL'], heads = params['HDS'], mlp_dim = params['MLP'], out_dims = len(params['y_labels']) )
         
-        optimizer = optim.Adam(model.parameters(), lr=params['LR'], weight_decay=params['WD'])
-        criterion = nn.MSELoss(reduction='none')
+        print(sum(p.numel() for p in model.parameters()))
+         
+        # optimizer = optim.Adam(model.parameters(), lr=params['LR'], weight_decay=params['WD'])
+        # criterion = nn.MSELoss(reduction='none')
 
 
-        base_path =os.path.dirname(params['data_path'])+f"/model_benchmark/oC/{params['model_name']}/run_{params['ID']}"
-        os.makedirs(base_path, exist_ok=True)
-        print(base_path)
+        # base_path =os.path.dirname(params['data_path'])+f"/model_benchmark/oC/{params['model_name']}/run_{params['ID']}"
+        # os.makedirs(base_path, exist_ok=True)
+        # print(base_path)
         
         
             
         
-        train_losses, val_losses,val_r2_scores,final_save_path,best_epoch = train(model, optimizer, criterion, cal_loader, val_loader, 
-                                        num_epochs=params['num_epochs'], plot_fig=False,save_path=base_path)
+        # train_losses, val_losses,val_r2_scores,final_save_path,best_epoch = train(model, optimizer, criterion, cal_loader, val_loader, 
+        #                                 num_epochs=params['num_epochs'], plot_fig=False,save_path=base_path)
 
-        tl = torch.stack(train_losses).numpy()
-        vl = torch.stack(val_losses).numpy()
-        r2= np.array(val_r2_scores)
+        # tl = torch.stack(train_losses).numpy()
+        # vl = torch.stack(val_losses).numpy()
+        # r2= np.array(val_r2_scores)
 
-        # Plotting Training and Validation Losses
-        for i,y in enumerate(params['y_labels']):
-            fig, ax1 = plt.subplots(figsize=(12, 6))
-            ax1.set_xlabel('Epoch')
-            ax1.set_ylabel('Loss')
-            ax1.plot(tl[:,i], label=f'Training Loss',color='tab:blue')
-            ax1.plot(vl[:,i], label=f'Validation Loss',color='tab:orange')
-            ax1.legend(loc='upper right', bbox_to_anchor=(1, 0.9), fancybox=True, shadow=True, fontsize=12)
+        # # Plotting Training and Validation Losses
+        # for i,y in enumerate(params['y_labels']):
+        #     fig, ax1 = plt.subplots(figsize=(12, 6))
+        #     ax1.set_xlabel('Epoch')
+        #     ax1.set_ylabel('Loss')
+        #     ax1.plot(tl[:,i], label=f'Training Loss',color='tab:blue')
+        #     ax1.plot(vl[:,i], label=f'Validation Loss',color='tab:orange')
+        #     ax1.legend(loc='upper right', bbox_to_anchor=(1, 0.9), fancybox=True, shadow=True, fontsize=12)
 
-            ax2 = ax1.twinx()
-            ax2.tick_params(axis='y', labelcolor='tab:green')
-            ax2.set_ylabel('R2 Score', color='tab:green')
-            ax2.plot(r2[:,i], label=f'R2 Score', linestyle='--',color='tab:green')
-            ax2.legend(loc='upper right', bbox_to_anchor=(1, 0.75), fancybox=True, shadow=True, fontsize=12)
+        #     ax2 = ax1.twinx()
+        #     ax2.tick_params(axis='y', labelcolor='tab:green')
+        #     ax2.set_ylabel('R2 Score', color='tab:green')
+        #     ax2.plot(r2[:,i], label=f'R2 Score', linestyle='--',color='tab:green')
+        #     ax2.legend(loc='upper right', bbox_to_anchor=(1, 0.75), fancybox=True, shadow=True, fontsize=12)
 
-            ax1.grid(True)
-            plt.title(f'Training performances for:  {y}')
-            # plt.show(block=False)
+        #     ax1.grid(True)
+        #     plt.title(f'Training performances for:  {y}')
+        #     # plt.show(block=False)
             
-            pdf_path =base_path+ f"/RMSE_{params['dataset_type']}.pdf"
-            plt.savefig(pdf_path, format='pdf')
+        #     pdf_path =base_path+ f"/RMSE_{params['dataset_type']}.pdf"
+        #     plt.savefig(pdf_path, format='pdf')
 
-            # pickle_path = base_path + f"/RMSE_{params['dataset_type']}.pkl"
-            # with open(pickle_path, 'wb') as f:
-            #     pickle.dump(fig, f)
+        #     # pickle_path = base_path + f"/RMSE_{params['dataset_type']}.pkl"
+        #     # with open(pickle_path, 'wb') as f:
+        #     #     pickle.dump(fig, f)
         
 
 
-            y_pred,Y=test(model,final_save_path,val_loader)
+        #     y_pred,Y=test(model,final_save_path,val_loader)
             
-            CCC =ccc(y_pred,Y)
-            r2=r2_score(y_pred, Y)
-            rmsep=RMSEP(y_pred, Y)
+        #     CCC =ccc(y_pred,Y)
+        #     r2=r2_score(y_pred, Y)
+        #     rmsep=RMSEP(y_pred, Y)
             
 
                     
-            metrics_dict = {
-            'dataset_type': params['dataset_type'],
-            'data_path': params['data_path'], 
-            'mean': params['mean'],
-            'std': params['std'],
-            'y_labels': params['y_labels'],
-            'num_epochs': params['num_epochs'],
-            "batch_size": params['batch_size'],
-            "seed": params['seed'] ,
-            "LR": params['LR'] ,
-            "WD": params['WD'] ,
-            "slope":params['slope'],
-            "offset":params['offset'],
-            "noise": params['noise'],
-            "shift":params['shift'],
-            "CCC": CCC,
-            "r2": r2,
-            "rmsep":rmsep,
-            "N parameters" : sum(p.numel() for p in model.parameters()),
-            "model_name": params["model_name"],
-            "best epoch": best_epoch
+        #     metrics_dict = {
+        #     'dataset_type': params['dataset_type'],
+        #     'data_path': params['data_path'], 
+        #     'mean': params['mean'],
+        #     'std': params['std'],
+        #     'y_labels': params['y_labels'],
+        #     'num_epochs': params['num_epochs'],
+        #     "batch_size": params['batch_size'],
+        #     "seed": params['seed'] ,
+        #     "LR": params['LR'] ,
+        #     "WD": params['WD'] ,
+        #     "slope":params['slope'],
+        #     "offset":params['offset'],
+        #     "noise": params['noise'],
+        #     "shift":params['shift'],
+        #     "CCC": CCC,
+        #     "r2": r2,
+        #     "rmsep":rmsep,
+        #     "N parameters" : sum(p.numel() for p in model.parameters()),
+        #     "model_name": params["model_name"],
+        #     "best epoch": best_epoch
             
-            }
+        #     }
                 
-            with open((base_path+'/metrics.text'), 'w') as f:
-                for key, value in metrics_dict.items():
-                    f.write(f"{key}: {value}\n")
+        #     with open((base_path+'/metrics.text'), 'w') as f:
+        #         for key, value in metrics_dict.items():
+        #             f.write(f"{key}: {value}\n")
 
         
-        print("CCC: %5.5f, R2: %5.5f, RMSEP: %5.5f"%(CCC, r2, rmsep))
+        # print("CCC: %5.5f, R2: %5.5f, RMSEP: %5.5f"%(CCC, r2, rmsep))
 
-        fig, ax = plt.subplots()
+        # fig, ax = plt.subplots()
         
-        # Scatter plot of X vs Y
-        plt.scatter(Y,y_pred,edgecolors='k',alpha=0.5)
+        # # Scatter plot of X vs Y
+        # plt.scatter(Y,y_pred,edgecolors='k',alpha=0.5)
         
-        # Plot of the 45 degree line
-        plt.plot([Y.min()-1,Y.max()+1],[Y.min()-1,Y.max()+1],'r')
+        # # Plot of the 45 degree line
+        # plt.plot([Y.min()-1,Y.max()+1],[Y.min()-1,Y.max()+1],'r')
         
-        plt.text(0.05, 0.95, f'CCC: {CCC:.2f}\nR²: {r2:.2f}', 
-                    transform=plt.gca().transAxes, fontsize=12,
-                    verticalalignment='top', horizontalalignment='left',
-                    bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.5'),
-                    color='red', fontweight='bold', fontfamily='serif')
+        # plt.text(0.05, 0.95, f'CCC: {CCC:.2f}\nR²: {r2:.2f}', 
+        #             transform=plt.gca().transAxes, fontsize=12,
+        #             verticalalignment='top', horizontalalignment='left',
+        #             bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.5'),
+        #             color='red', fontweight='bold', fontfamily='serif')
         
-        ax.set_xlabel('Real Values')
-        ax.set_ylabel('Predicted Values')
-        ax.set_title(f'Predicted vs Real Values for Oc(log x + 1)')
+        # ax.set_xlabel('Real Values')
+        # ax.set_ylabel('Predicted Values')
+        # ax.set_title(f'Predicted vs Real Values for Oc(log x + 1)')
         
-        plt.tight_layout()
-        plt.grid()
-        # fig.show(block=False)
+        # plt.tight_layout()
+        # plt.grid()
+        # # fig.show(block=False)
         
-        pdf_path = base_path +f"/predicted_vs_observed_{params['dataset_type']}.pdf"
-        plt.savefig(pdf_path, format='pdf')
+        # pdf_path = base_path +f"/predicted_vs_observed_{params['dataset_type']}.pdf"
+        # plt.savefig(pdf_path, format='pdf')
 
-        # pickle_path = base_path + f"/predicted_vs_observed_{params['dataset_type']}.pkl"
-        # with open(pickle_path, 'wb') as f:
-        #     pickle.dump(fig, f)
+        # # pickle_path = base_path + f"/predicted_vs_observed_{params['dataset_type']}.pkl"
+        # # with open(pickle_path, 'wb') as f:
+        # #     pickle.dump(fig, f)
         
 
         
-        fig, ax = plt.subplots()
-        hexbin = ax.hexbin(Y.squeeze().numpy(), y_pred.squeeze().numpy(), gridsize=50, cmap='viridis', mincnt=1)
-        cb = fig.colorbar(hexbin, ax=ax, orientation='vertical')
-        cb.set_label('Density')
-        lims = [np.min([Y.numpy(), y_pred.numpy()]), np.max([Y.numpy(), y_pred.numpy()])]
-        ax.plot(lims, lims, 'k-', label= params['y_labels'])  
+        # fig, ax = plt.subplots()
+        # hexbin = ax.hexbin(Y.squeeze().numpy(), y_pred.squeeze().numpy(), gridsize=50, cmap='viridis', mincnt=1)
+        # cb = fig.colorbar(hexbin, ax=ax, orientation='vertical')
+        # cb.set_label('Density')
+        # lims = [np.min([Y.numpy(), y_pred.numpy()]), np.max([Y.numpy(), y_pred.numpy()])]
+        # ax.plot(lims, lims, 'k-', label= params['y_labels'])  
         
-        plt.text(0.05, 0.95, f'CCC: {CCC:.2f}\nR²: {r2:.2f}', 
-                    transform=plt.gca().transAxes, fontsize=12,
-                    verticalalignment='top', horizontalalignment='left',
-                    bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.5'),
-                    color='red', fontweight='bold', fontfamily='serif')
+        # plt.text(0.05, 0.95, f'CCC: {CCC:.2f}\nR²: {r2:.2f}', 
+        #             transform=plt.gca().transAxes, fontsize=12,
+        #             verticalalignment='top', horizontalalignment='left',
+        #             bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.5'),
+        #             color='red', fontweight='bold', fontfamily='serif')
         
-        ax.set_xlabel('Real Values')
-        ax.set_ylabel('Predicted Values')
-        ax.set_title(f'Predicted vs Real Values for Oc (log x + 1)')
+        # ax.set_xlabel('Real Values')
+        # ax.set_ylabel('Predicted Values')
+        # ax.set_title(f'Predicted vs Real Values for Oc (log x + 1)')
         
         
-        # all_labels = ["oc_usda.c729_w.pct","na.ext_usda.a726_cmolc.kg","clay.tot_usda.a334_w.pct", "k.ext_usda.a725_cmolc.kg", "ph.h2o_usda.a268_index" ]  #
-        # if len(params['y_labels'])==1:
-        #     target_index = all_labels.index(params['y_labels'][0])
-        # else:
-        #     target_index=len(params['y_labels'])
+        # # all_labels = ["oc_usda.c729_w.pct","na.ext_usda.a726_cmolc.kg","clay.tot_usda.a334_w.pct", "k.ext_usda.a725_cmolc.kg", "ph.h2o_usda.a268_index" ]  #
+        # # if len(params['y_labels'])==1:
+        # #     target_index = all_labels.index(params['y_labels'][0])
+        # # else:
+        # #     target_index=len(params['y_labels'])
         
-        plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),
-                    fancybox=True, shadow=True, ncol=5, fontsize=12) #labelcolor =default_colors[target_index]
-        plt.tight_layout()
-        plt.grid()
-        # fig.show()
+        # plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),
+        #             fancybox=True, shadow=True, ncol=5, fontsize=12) #labelcolor =default_colors[target_index]
+        # plt.tight_layout()
+        # plt.grid()
+        # # fig.show()
         
-        hexbin_pdf_path = base_path+ f"/fig_hexbin.pdf"
-        plt.savefig(hexbin_pdf_path, format='pdf')
+        # hexbin_pdf_path = base_path+ f"/fig_hexbin.pdf"
+        # plt.savefig(hexbin_pdf_path, format='pdf')
 
-        # Save the figure object using pickle
-        # hexbin_pickle_path = base_path+ f"/fig_hexbin.pkl"
-        # with open(hexbin_pickle_path, 'wb') as f:
-        #     pickle.dump(fig, f)    
+        # # Save the figure object using pickle
+        # # hexbin_pickle_path = base_path+ f"/fig_hexbin.pkl"
+        # # with open(hexbin_pickle_path, 'wb') as f:
+        # #     pickle.dump(fig, f)    
             
-        del augmentation
-        del spectral_data
-        del train_dataset, test_dataset, cal_dataset, val_dataset
-        del cal_loader, val_loader, test_loader
-        del mean, std
-        del model, optimizer, criterion
-        del train_losses, val_losses, val_r2_scores, final_save_path
+        # del augmentation
+        # del spectral_data
+        # del train_dataset, test_dataset, cal_dataset, val_dataset
+        # del cal_loader, val_loader, test_loader
+        # del mean, std
+        # del model, optimizer, criterion
+        # del train_losses, val_losses, val_r2_scores, final_save_path
 
-        # Optional: Clear PyTorch cache
-        if torch.cuda.is_available():
-            torch.cuda.empty_cache()
+        # # Optional: Clear PyTorch cache
+        # if torch.cuda.is_available():
+        #     torch.cuda.empty_cache()
 
-        print(f"Finished cleaning variables for run_{params['ID']}")
+        # print(f"Finished cleaning variables for run_{params['ID']}")
 
