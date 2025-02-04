262a33fdfb9aa017e924675cb4cd9a3bd6f95acc
florent-abdelghafour <124244994+florent-abdelghafour@users.noreply.github.com>
2025-02-05 00:12:39 +0100

    Update lunch_ViT_batch.sh

--- lunch_ViT_batch.sh
+++ lunch_ViT_batch.sh
@@ -1,8 +1,11 @@
 
-PYTHON_SCRIPT="./train_ViT1D_mango_batch_argparse.py"
+# PYTHON_SCRIPT="./train_ViT1D_mango_batch_argparse.py"
+PYTHON_SCRIPT="./train_ViT1D_batch_argparse.py"
 
 # Specify the path to your JSON configuration file
-CONFIG_FILE="./data/dataset/Mango/config/_ViT1D_Mango_optim(dm_mango).json"
+# CONFIG_FILE="./data/dataset/Mango/config/_ViT1D_Mango_optim(dm_mango).json"
+CONFIG_FILE="./data/dataset/ossl/config/ViT1D/config_batch.json"
+
 
 
 # Run the Python script with the JSON config file as an argument
